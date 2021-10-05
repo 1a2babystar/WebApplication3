e01dbea4324d7b83cb5ba98842b3358282143778
1a2babystar <55686526+1a2babystar@users.noreply.github.com>
2021-10-05 16:57:27 +0900

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var noticeli = Noticeacts.GetNotices();
+            return View(noticeli);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Createhandler()
+        {
+            return View("create");
+        }
+
+        public IActionResult Createsubmit(string title, string writer, string content)
+        {
+            string time = DateTime.Now.ToString("HH:mm:ss tt");
+            Notice notice = new Notice(title, writer, content, time);
+            Noticeacts.CreateNotice(notice);
+            var noticeli = Noticeacts.GetNotices();
+            return View("index", noticeli);
+        }
+
+        public IActionResult Detail(int Id)
+        {
+            Notice notice = Noticeacts.ShowDetail(Id);
+            return View(notice);
+        }
+    }
+}
---
+++ Models/Notice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Notice
+    {
+        public Notice(string title, string writer, string contents, string date)
+        {
+            this.title = title;
+            this.writer = writer;
+            this.contents = contents;
+            this.date = date;
+        }
+        public string writer { get; set; }
+        public string title { get; set; }
+        public string date { get; set; }
+        public string contents { get; set; }
+    }
+}
